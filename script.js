72f6247abe0e1eb714ee32ab98f4f7d57c4b996b
oezhasan <hasan98oez@gmail.com>
2022-01-22 23:50:15 +0100

    css adjsut

--- script.js
+++ script.js
@@ -1,50 +1,53 @@
 const navLinks = document.querySelectorAll(".nav-link");
 const sections = document.querySelectorAll("section");
-const darkMode = document.querySelector(".toggle-darkmode")
+const darkMode = document.querySelector("#toggle-darkmode");
 
-console.log(sections)
-navLinksArray = Array.from(navLinks)
+console.log(sections);
+navLinksArray = Array.from(navLinks);
 sectionsArray = Array.from(sections);
 
-darkMode.addEventListener("click", function(){
-    document.body.classList.toggle("dark-mode");
-})
-
-navLinksArray.forEach((link)=>{
-
-    link.addEventListener("click", function(link){
-        updateNavbar(this)
-
-        this.classList.add("nav-link-selected");   
-    })
-    
+darkMode.addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode");
 });
-console.log(sectionsArray[0])
 
+window.onscroll = () => {
+  let current = " ";
 
-window.onscroll = function(){
-
-    
-    if (sectionsArray[0].getBoundingClientRect().bottom > 0){
-        updateNavbar(navLinksArray[0]);
+  sectionsArray.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute("id");
     }
-    else if(sectionsArray[1].getBoundingClientRect().bottom > 0){
-        updateNavbar(navLinksArray[1]);
-    }
-    else if(sectionsArray[2].getBoundingClientRect().bottom > 0){
-        updateNavbar(navLinksArray[2]);
-    
-    }
-}
+  });
 
+  updateNavbar(current);
 
-function updateNavbar(current){
-    navLinksArray.forEach((navLink) => {
-        if (navLink === current){
-            navLink.classList.add("nav-link-selected");   
-        }
-        else{
-            navLink.classList.remove("nav-link-selected");   
-        }
-    })
+  navLinksArray.forEach((li) => {
+    li.classList.remove("nav-link-selected");
+
+    if (li.href.includes(current)) {
+      li.classList.add("nav-link-selected");
+    }
+  });
+
+  // if (sectionsArray[0].getBoundingClientRect().bottom > 0){
+  //     updateNavbar(navLinksArray[0]);
+  // }
+  // else if(sectionsArray[1].getBoundingClientRect().bottom > 0){
+  //     updateNavbar(navLinksArray[1]);
+  // }
+  // else if(sectionsArray[2].getBoundingClientRect().bottom > 0){
+  //     updateNavbar(navLinksArray[2]);
+
+  // }
+};
+
+function updateNavbar(current) {
+  navLinksArray.forEach((navLink) => {
+    if (navLink === current) {
+      navLink.classList.add("nav-link-selected");
+    } else {
+      navLink.classList.remove("nav-link-selected");
+    }
+  });
 }
